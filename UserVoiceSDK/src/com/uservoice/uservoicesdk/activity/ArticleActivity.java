62b82839990fb2f7ed328c71dd22ca03feb66233
Austin Taylor <austin.taylor@gmail.com>
2013-05-02 14:50:51 -0400

    Fix video embeds in articles

--- UserVoiceSDK/src/com/uservoice/uservoicesdk/activity/ArticleActivity.java
+++ UserVoiceSDK/src/com/uservoice/uservoicesdk/activity/ArticleActivity.java
@@ -1,14 +1,18 @@
 package com.uservoice.uservoicesdk.activity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings.PluginState;
 import android.webkit.WebView;
 
 import com.uservoice.uservoicesdk.R;
 import com.uservoice.uservoicesdk.Session;
 import com.uservoice.uservoicesdk.model.Article;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class ArticleActivity extends Activity {
 
 	@Override
@@ -18,10 +22,13 @@ public class ArticleActivity extends Activity {
 		setContentView(R.layout.article_layout);
 		Article article = Session.getInstance().getArticle();
 		System.out.println(article.getAnswerHtml());
-		String styles = "img { width: 100%; }";
+		String styles = "iframe, img { width: 100%; }";
 	    String html = String.format("<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"http://uservoice.com/stylesheets/vendor/typeset.css\"/><style>%s</style></head><body class=\"typeset\" style=\"font-family: sans-serif; margin: 1em\"><h3>%s</h3>%s</body></html>", styles, article.getQuestion(), article.getAnswerHtml());
 
 		WebView webview = (WebView) findViewById(R.id.webview);
+		webview.setWebChromeClient(new WebChromeClient());
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.getSettings().setPluginState(PluginState.ON);
 		webview.loadUrl(String.format("data:text/html;charset=utf-8,%s", Uri.encode(html)));
 	}
 }
