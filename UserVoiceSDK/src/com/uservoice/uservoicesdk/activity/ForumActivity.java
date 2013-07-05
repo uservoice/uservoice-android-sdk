43eece327fffb54e7d94aac290771d986aa9729c
Austin Taylor <austin.taylor@gmail.com>
2013-07-05 18:05:10 -0400

    Don't try to load ideas on sign-in if the forum isn't loaded yet

--- UserVoiceSDK/src/com/uservoice/uservoicesdk/activity/ForumActivity.java
+++ UserVoiceSDK/src/com/uservoice/uservoicesdk/activity/ForumActivity.java
@@ -38,227 +38,229 @@ import com.uservoice.uservoicesdk.ui.Utils;
 
 public class ForumActivity extends BaseListActivity implements SearchActivity {
 
-	private List<Suggestion> suggestions;
-	private Forum forum;
+    private List<Suggestion> suggestions;
+    private Forum forum;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setTitle(R.string.uv_feedback_forum);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.uv_feedback_forum);
 
-		suggestions = new ArrayList<Suggestion>();
+        suggestions = new ArrayList<Suggestion>();
 
-		getListView().setDivider(null);
-		setListAdapter(new PaginatedAdapter<Suggestion>(this, R.layout.suggestion_item, suggestions) {
+        getListView().setDivider(null);
+        setListAdapter(new PaginatedAdapter<Suggestion>(this, R.layout.suggestion_item, suggestions) {
 
-			@Override
-			public int getViewTypeCount() {
-				return super.getViewTypeCount() + 2;
-			}
-			
-			@Override
-			public boolean isEnabled(int position) {
-				return getItemViewType(position) == 2 || super.isEnabled(position);
-			}
-			
-			@Override
-			public int getCount() {
-				return super.getCount() + 2;
-			}
+            @Override
+            public int getViewTypeCount() {
+                return super.getViewTypeCount() + 2;
+            }
 
-			@Override
-			public int getItemViewType(int position) {
-				if (position == 0)
-					return 2;
-				if (position == 1)
-					return 3;
-				return super.getItemViewType(position - 2);
-			}
+            @Override
+            public boolean isEnabled(int position) {
+                return getItemViewType(position) == 2 || super.isEnabled(position);
+            }
 
-			@Override
-			public Object getItem(int position) {
-				return super.getItem(position - 2);
-			}
+            @Override
+            public int getCount() {
+                return super.getCount() + 2;
+            }
 
-			@Override
-			public View getView(int position, View convertView, ViewGroup parent) {
-				int type = getItemViewType(position);
-				if (type == 2 || type == 3) {
-					View view = convertView;
-					if (view == null) {
-						if (type == 2) {
-							view = getLayoutInflater().inflate(R.layout.text_item, null);
-							TextView text = (TextView) view.findViewById(R.id.text);
-							text.setText(R.string.uv_post_an_idea);
-							view.findViewById(R.id.divider).setVisibility(View.GONE);
-							view.findViewById(R.id.text2).setVisibility(View.GONE);
-						} else if (type == 3) {
-							view = getLayoutInflater().inflate(R.layout.header_item_light, null);
-							TextView text = (TextView) view.findViewById(R.id.header_text);
-							text.setText(R.string.uv_idea_text_heading);
-						}
-					}
-					return view;
-				} else {
-					return super.getView(position, convertView, parent);
-				}
-			}
+            @Override
+            public int getItemViewType(int position) {
+                if (position == 0)
+                    return 2;
+                if (position == 1)
+                    return 3;
+                return super.getItemViewType(position - 2);
+            }
 
-			@Override
-			protected void customizeLayout(View view, Suggestion model) {
-				TextView textView = (TextView) view.findViewById(R.id.suggestion_title);
-				textView.setText(model.getTitle());
+            @Override
+            public Object getItem(int position) {
+                return super.getItem(position - 2);
+            }
 
-				textView = (TextView) view.findViewById(R.id.subscriber_count);
-				textView.setText(String.valueOf(model.getNumberOfSubscribers()));
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                int type = getItemViewType(position);
+                if (type == 2 || type == 3) {
+                    View view = convertView;
+                    if (view == null) {
+                        if (type == 2) {
+                            view = getLayoutInflater().inflate(R.layout.text_item, null);
+                            TextView text = (TextView) view.findViewById(R.id.text);
+                            text.setText(R.string.uv_post_an_idea);
+                            view.findViewById(R.id.divider).setVisibility(View.GONE);
+                            view.findViewById(R.id.text2).setVisibility(View.GONE);
+                        } else if (type == 3) {
+                            view = getLayoutInflater().inflate(R.layout.header_item_light, null);
+                            TextView text = (TextView) view.findViewById(R.id.header_text);
+                            text.setText(R.string.uv_idea_text_heading);
+                        }
+                    }
+                    return view;
+                } else {
+                    return super.getView(position, convertView, parent);
+                }
+            }
 
-				textView = (TextView) view.findViewById(R.id.suggestion_status);
-				View colorView = view.findViewById(R.id.suggestion_status_color);
-				if (model.getStatus() == null) {
-					textView.setVisibility(View.GONE);
-					colorView.setVisibility(View.GONE);
-				} else {
-					int color = Color.parseColor(model.getStatusColor());
-					textView.setVisibility(View.VISIBLE);
-					textView.setTextColor(color);
-					textView.setText(model.getStatus().toUpperCase(Locale.getDefault()));
-					colorView.setVisibility(View.VISIBLE);
-					colorView.setBackgroundColor(color);
-				}
-			}
+            @Override
+            protected void customizeLayout(View view, Suggestion model) {
+                TextView textView = (TextView) view.findViewById(R.id.suggestion_title);
+                textView.setText(model.getTitle());
 
-			@Override
-			public void loadPage(int page, Callback<List<Suggestion>> callback) {
-				Suggestion.loadSuggestions(forum, page, callback);
-			}
+                textView = (TextView) view.findViewById(R.id.subscriber_count);
+                textView.setText(String.valueOf(model.getNumberOfSubscribers()));
 
-			@Override
-			public RestTask search(final String query, final Callback<List<Suggestion>> callback) {
-				return Suggestion.searchSuggestions(forum, query, new Callback<List<Suggestion>>() {
-					
-					@Override
-					public void onModel(List<Suggestion> model) {
-						Babayaga.track(Babayaga.Event.SEARCH_IDEAS, query, model);
-						callback.onModel(model);
-					}
-					
-					@Override
-					public void onError(RestResult error) {
-						callback.onError(error);
-					}
-				});
-			}
+                textView = (TextView) view.findViewById(R.id.suggestion_status);
+                View colorView = view.findViewById(R.id.suggestion_status_color);
+                if (model.getStatus() == null) {
+                    textView.setVisibility(View.GONE);
+                    colorView.setVisibility(View.GONE);
+                } else {
+                    int color = Color.parseColor(model.getStatusColor());
+                    textView.setVisibility(View.VISIBLE);
+                    textView.setTextColor(color);
+                    textView.setText(model.getStatus().toUpperCase(Locale.getDefault()));
+                    colorView.setVisibility(View.VISIBLE);
+                    colorView.setBackgroundColor(color);
+                }
+            }
 
-			@Override
-			public int getTotalNumberOfObjects() {
-				return forum.getNumberOfOpenSuggestions();
-			}
-		});
+            @Override
+            public void loadPage(int page, Callback<List<Suggestion>> callback) {
+                Suggestion.loadSuggestions(forum, page, callback);
+            }
 
-		getListView().setOnScrollListener(new PaginationScrollListener(getModelAdapter()) {
-			@Override
-			public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-				if (forum != null)
-					super.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
-			}
-		});
+            @Override
+            public RestTask search(final String query, final Callback<List<Suggestion>> callback) {
+                return Suggestion.searchSuggestions(forum, query, new Callback<List<Suggestion>>() {
 
-		getListView().setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				if (position == 0) {
-					startActivity(new Intent(ForumActivity.this, PostIdeaActivity.class));
-				} else if (position != 1) {
-					Suggestion suggestion = (Suggestion) getModelAdapter().getItem(position);
-					Session.getInstance().setSuggestion(suggestion);
-					SuggestionDialogFragment dialog = new SuggestionDialogFragment(suggestion);
-					dialog.show(getSupportFragmentManager(), "SuggestionDialogFragment");
-				}
-			}
-		});
+                    @Override
+                    public void onModel(List<Suggestion> model) {
+                        Babayaga.track(Babayaga.Event.SEARCH_IDEAS, query, model);
+                        callback.onModel(model);
+                    }
 
-		new InitManager(this, new Runnable() {
-			@Override
-			public void run() {
-				loadForum();
-				Session.getInstance().setSignInListener(new Runnable() {
-					@Override
-					public void run() {
-						getModelAdapter().reload();
-					}
-				});
-			}
-		}).init();
-	}
+                    @Override
+                    public void onError(RestResult error) {
+                        callback.onError(error);
+                    }
+                });
+            }
 
-	@SuppressLint("NewApi")
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.forum, menu);
-		if (Utils.hasActionBar()) {
-			menu.findItem(R.id.menu_search).setOnActionExpandListener(new SearchExpandListener(this));
-			SearchView search = (SearchView) menu.findItem(R.id.menu_search).getActionView();
-			search.setOnQueryTextListener(new SearchQueryListener(this));
-		} else {
-			menu.findItem(R.id.menu_search).setVisible(false);
-		}
-		menu.findItem(R.id.new_idea).setVisible(Session.getInstance().getConfig().shouldShowPostIdea());
-		return true;
-	}
+            @Override
+            public int getTotalNumberOfObjects() {
+                return forum.getNumberOfOpenSuggestions();
+            }
+        });
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		if (item.getItemId() == R.id.new_idea) {
-			startActivity(new Intent(this, PostIdeaActivity.class));
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+        getListView().setOnScrollListener(new PaginationScrollListener(getModelAdapter()) {
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (forum != null)
+                    super.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
+            }
+        });
 
-	@Override
-	protected void onStop() {
-		Session.getInstance().setSignInListener(null);
-		super.onStop();
-	}
+        getListView().setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    startActivity(new Intent(ForumActivity.this, PostIdeaActivity.class));
+                } else if (position != 1) {
+                    Suggestion suggestion = (Suggestion) getModelAdapter().getItem(position);
+                    Session.getInstance().setSuggestion(suggestion);
+                    SuggestionDialogFragment dialog = new SuggestionDialogFragment(suggestion);
+                    dialog.show(getSupportFragmentManager(), "SuggestionDialogFragment");
+                }
+            }
+        });
 
-	private void loadForum() {
-		if (Session.getInstance().getForum() != null) {
-			forum = Session.getInstance().getForum();
-			Babayaga.track(Babayaga.Event.VIEW_FORUM, forum.getId());
-			setTitle(forum.getName());
-			getModelAdapter().loadMore();
-			return;
-		}
-		Forum.loadForum(Session.getInstance().getConfig().getForumId(), new DefaultCallback<Forum>(this) {
-			@Override
-			public void onModel(Forum model) {
-				Session.getInstance().setForum(model);
-				forum = model;
-				setTitle(forum.getName());
-				getModelAdapter().loadMore();
-			}
-		});
-	}
-	
-	@Override
-	public SearchAdapter<?> getSearchAdapter() {
-		return getModelAdapter();
-	}
+        new InitManager(this, new Runnable() {
+            @Override
+            public void run() {
+                loadForum();
+                Session.getInstance().setSignInListener(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (forum != null) {
+                            getModelAdapter().reload();
+                        }
+                    }
+                });
+            }
+        }).init();
+    }
 
-	@SuppressWarnings("unchecked")
-	public PaginatedAdapter<Suggestion> getModelAdapter() {
-		return (PaginatedAdapter<Suggestion>) getListAdapter();
-	}
+    @SuppressLint("NewApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forum, menu);
+        if (Utils.hasActionBar()) {
+            menu.findItem(R.id.menu_search).setOnActionExpandListener(new SearchExpandListener(this));
+            SearchView search = (SearchView) menu.findItem(R.id.menu_search).getActionView();
+            search.setOnQueryTextListener(new SearchQueryListener(this));
+        } else {
+            menu.findItem(R.id.menu_search).setVisible(false);
+        }
+        menu.findItem(R.id.new_idea).setVisible(Session.getInstance().getConfig().shouldShowPostIdea());
+        return true;
+    }
 
-	@Override
-	public void showSearch() {
-	}
-	
-	@Override
-	public void hideSearch() {
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.new_idea) {
+            startActivity(new Intent(this, PostIdeaActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	public void suggestionUpdated(Suggestion model) {
-		getModelAdapter().notifyDataSetChanged();
-	}
+    @Override
+    protected void onStop() {
+        Session.getInstance().setSignInListener(null);
+        super.onStop();
+    }
+
+    private void loadForum() {
+        if (Session.getInstance().getForum() != null) {
+            forum = Session.getInstance().getForum();
+            Babayaga.track(Babayaga.Event.VIEW_FORUM, forum.getId());
+            setTitle(forum.getName());
+            getModelAdapter().loadMore();
+            return;
+        }
+        Forum.loadForum(Session.getInstance().getConfig().getForumId(), new DefaultCallback<Forum>(this) {
+            @Override
+            public void onModel(Forum model) {
+                Session.getInstance().setForum(model);
+                forum = model;
+                setTitle(forum.getName());
+                getModelAdapter().loadMore();
+            }
+        });
+    }
+
+    @Override
+    public SearchAdapter<?> getSearchAdapter() {
+        return getModelAdapter();
+    }
+
+    @SuppressWarnings("unchecked")
+    public PaginatedAdapter<Suggestion> getModelAdapter() {
+        return (PaginatedAdapter<Suggestion>) getListAdapter();
+    }
+
+    @Override
+    public void showSearch() {
+    }
+
+    @Override
+    public void hideSearch() {
+    }
+
+    public void suggestionUpdated(Suggestion model) {
+        getModelAdapter().notifyDataSetChanged();
+    }
 }
