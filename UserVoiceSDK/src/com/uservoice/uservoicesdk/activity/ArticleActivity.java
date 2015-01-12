cc137734bae7996eddc8dbd922e148ce8e3afd2e
Austin Taylor <austin.taylor@gmail.com>
2015-01-12 17:40:51 -0500

    Fix bug that prevented browsing articles

--- UserVoiceSDK/src/com/uservoice/uservoicesdk/activity/ArticleActivity.java
+++ UserVoiceSDK/src/com/uservoice/uservoicesdk/activity/ArticleActivity.java
@@ -4,11 +4,15 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.uservoice.uservoicesdk.R;
@@ -20,6 +24,8 @@ import com.uservoice.uservoicesdk.ui.Utils;
 
 public class ArticleActivity extends SearchActivity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +33,15 @@ public class ArticleActivity extends SearchActivity {
         setContentView(R.layout.uv_article_layout);
         final Article article = getIntent().getParcelableExtra("article");
         setTitle(article.getTitle());
-        WebView webView = (WebView) findViewById(R.id.uv_webview);
+        webView = (WebView) findViewById(R.id.uv_webview);
+        final View helpfulSection = findViewById(R.id.uv_helpful_section);
         Utils.displayArticle(webView, article, this);
         findViewById(R.id.uv_container).setBackgroundColor(Utils.isDarkTheme(this) ? Color.BLACK : Color.WHITE);
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                findViewById(R.id.uv_helpful_section).setVisibility(View.VISIBLE);
+                helpfulSection.setVisibility(View.VISIBLE);
             }
         });
         findViewById(R.id.uv_helpful_button).setOnClickListener(new View.OnClickListener() {
@@ -53,6 +60,13 @@ public class ArticleActivity extends SearchActivity {
         });
 
         Babayaga.track(Babayaga.Event.VIEW_ARTICLE, article.getId());
+    }
+
+    @Override
+    public ListView getListView() {
+        // This is called by setupScopedSearch(menu) to make sure the list is added,
+        // but we don't want a list. Should probably refactor this somehow.
+        return null;
     }
 
     @Override
@@ -75,8 +89,7 @@ public class ArticleActivity extends SearchActivity {
     @Override
     public void finish() {
         // This is what you have to do to make it stop the flash player
-        WebView webview = (WebView) findViewById(R.id.uv_webview);
-        webview.loadData("", "text/html", "utf-8");
+        webView.loadData("", "text/html", "utf-8");
         super.finish();
     }
 }
