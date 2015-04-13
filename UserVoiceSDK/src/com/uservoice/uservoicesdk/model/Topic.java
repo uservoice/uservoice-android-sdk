f3f9c88fae4617e6f776539587e9eab20de4ac09
Austin Taylor <austin.taylor@gmail.com>
2015-04-13 17:58:57 -0400

    Imports

--- UserVoiceSDK/src/com/uservoice/uservoicesdk/model/Topic.java
+++ UserVoiceSDK/src/com/uservoice/uservoicesdk/model/Topic.java
@@ -4,7 +4,9 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
